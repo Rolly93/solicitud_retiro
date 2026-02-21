04d2d820c3f3c078428a995d2bf3ba16153df030
Rolly93 <129896901+Rolly93@users.noreply.github.com>
2026-02-21 15:10:08 -0600

    Refactor PDF service methods for better error handling

--- core/pdf_service.py
+++ core/pdf_service.py
@@ -7,7 +7,6 @@ class PDFService:
     def __init__(self):
         self.doc = None
         self.current_path = None
-        self.data = DataManager() 
         
     
     def is_empty_page(self):
@@ -31,7 +30,10 @@ class PDFService:
 
     def cargar_documento(self, ruta_pdf: str):
         """Carga un PDF y cierra el anterior si existe."""
-        if self.doc: self.doc.close()
+        try:
+            if hasattr(self,'doc') and self.doc: self.doc.close()
+        except Exception:
+            pass
         
         
         path = Path(ruta_pdf).resolve()
@@ -51,29 +53,24 @@ class PDFService:
         mat = fitz.Matrix(zoom, zoom)
         return page.get_pixmap(matrix=mat, alpha=False)
 
-    def escribir_campos(self, campos: dict, func_coords):
+    def escribir_campos(self, campos: dict, func_coords,nombre_pdf_activo):
         """
         Inserta texto en el PDF basado en un diccionario de valores
         y una función que provea las coordenadas.
         """
         if not self.doc: return self.is_empty_page()
-
+        
         page = self.doc.load_page(0)
-        tipo_solicitud = campos.get("tipo_solicitud", "").lower().replace(" ", "-")
-
+        tipo_solicitud = [s for  s in campos["tipo_solicitud"] ]
         for nombre, valor in campos.items():
             if nombre == "tipo_solicitud" or not valor:
                 continue
-            coords = func_coords(tipo_solicitud, nombre)
+            coords = func_coords(nombre_pdf_activo.lower().replace(" ", "-"), nombre)
             
             if coords and coords[0] is None: continue
-            
             x_mm , y_mm = coords    
             x_pts = x_mm * self.MM_TO_PTS
             y_pts = (y_mm * self.MM_TO_PTS) - 2
-            print("valor " , valor)
-            
-            
             page.insert_text(
                     (x_pts, y_pts), 
                     str(valor),
@@ -83,11 +80,18 @@ class PDFService:
                 )
 
 
-    def guardar_como(self, ruta_destino: str):
+    def guardar_como(self, ruta_destino: str) ->str:
         
-        ruta_destino = f"./documents/{ruta_destino}.pdf"
+        base =Path( "./documents")
+        ruta_destino =base  / ruta_destino
+        
+        ruta_destino.parent.mkdir(parents=True, exist_ok=True)
+        
         if self.doc:
-            self.doc.save(ruta_destino, garbage=4, deflate=True)
+            self.doc.save(str(ruta_destino), garbage=4, deflate=True)
+        
+        return str(ruta_destino)
+        
 
     def cerrar(self):
         if self.doc: self.doc.close()
